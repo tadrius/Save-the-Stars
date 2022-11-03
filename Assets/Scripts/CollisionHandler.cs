bbd250ba3fad3c7e0de330c2573190ba2e62adf9
tadrius <116924580+tadrius@users.noreply.github.com>
2022-11-03 13:52:04 -0500

    Add scene loading and collectible processing

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
@@ -9,20 +10,44 @@ public class CollisionHandler : MonoBehaviour
     public const string Finish = "Finish";
     public const string Friendly = "Friendly";
 
+    private int score = 0;
+
     private void OnCollisionEnter(Collision other) {
         switch (other.gameObject.tag) {
             case Collectible:
-                Debug.Log("Collided with a Collectible.");
-                break;
-            case Finish:
-                Debug.Log("Collided with a Finish.");
+                processCollectible(other.gameObject);
                 break;
             case Friendly:
-                Debug.Log("Collided with a Friendly.");
+                Debug.Log("Collided with Friendly.");
+                break;
+            case Finish:
+                LoadNextScene();
                 break;
             default:
-                Debug.Log("Collided.");
+                ReloadScene();
                 break;
         }
     }
+
+    private void processCollectible(GameObject gameObject) {
+        score++;
+        Debug.Log("Score: " + score);
+        gameObject.SetActive(false);
+    }
+
+    private void LoadNextScene() {
+        int nextSceneBuildIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        if (SceneManager.sceneCount > nextSceneBuildIndex) {
+            Debug.Log("Loading next scene.");
+            SceneManager.LoadScene(nextSceneBuildIndex + 1);
+        } else {
+            Debug.Log("No more scenes.");
+        }
+    }
+
+    private void ReloadScene() {
+        Debug.Log("Reloading scene.");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    
 }
